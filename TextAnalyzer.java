efb9cc6e5c2e713954401b0dd810a9d53ca60a14
fluffery <38757807+fluffery@users.noreply.github.com>
2018-05-05 17:43:49 +0200

    Update TextAnalyzer.java

--- TextAnalyzer.java
+++ TextAnalyzer.java
@@ -1,46 +1,55 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TextAnalyzer implements ITextAnalyzer {
 
-    public static final String[] SEARCH = {"fre","er","es","sie"};
+    public static final String[] SEARCH = {"fre", "er", "es", "sie"};
     public static File file = new File("/home/fluffy/Documents/Studium/Java/Blatt2.4/src/FIle1");
     public static final String PATH = file.getAbsolutePath();
-    String myString;
-    String REPLACE_ME = myString.replaceAll("[^a-zA-Z0-9]", "");
+    public String myString;
+    public String REPLACE_ME = myString.replaceAll("[^a-zA-Z0-9]", "");
 
     @Override
     public void run() {
-
         Scanner in = new Scanner(System.in);
         System.out.println("[r]lesen\t[a]Alphabetische Map\t[t]TermMap\t[q]beenden");
         String input = in.nextLine();
-        if (input.equalsIgnoreCase("r")) {
-            System.out.print("Dateiname (ENTER für default)");
-            try {
-                while (input!=null){
-                    if (file.exists()){
-                        String input2 = in.nextLine();
+        switch (input) {
+            case "r":
+                System.out.print("Dateiname (ENTER für default)");
+                String input2 = in.nextLine();
+                while (input2 != null) {
+                    if (file.exists()) {
+                        input2 = PATH;
                     }
-                    System.out.print("Dateiname: ");
-
-                } catch(FileNotFoundException ex) {
-                    System.out.println("Unable to open file ");
-
                 }
+                File file = new File(input2);
+                try (Scanner in2 = new Scanner(file)) {
+                    ArrayList<String> x = readWords(in2);
+                    System.out.println("Dateiname: " + input2);
+
+                } catch (FileNotFoundException e) {
+                    System.out.println(e);
                 }
+                break;
+            case "a":
+                HashMap<String, ArrayList<String>> alpha = MapBuilder.buildAlphaMap(readWords(new Scanner(new File(input2))));
+                printWords(alpha);
+                break;
+            case "t":
+                System.out.println("Suchbegriffe eingeben (ENTER für default):");
 
+            case "q":
 
-        in.close();
-    }}
+        }
+    }
 
     @Override
     public ArrayList<String> readWords(Scanner scanner) throws FileNotFoundException {
-        ArrayList<String> dict = new ArrayList<>();
+        ArrayList<String> dict = new ArrayList<String>();
         scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             dict.add(scanner.next());
@@ -50,23 +59,46 @@ public class TextAnalyzer implements ITextAnalyzer {
 
     @Override
     public ArrayList<String> modifyWords(ArrayList<String> wordList) {
-        return modifyWords(wordList,REPLACE_ME);
+        return modifyWords(wordList, REPLACE_ME);
     }
 
     @Override
     public ArrayList<String> modifyWords(ArrayList<String> wordList, String regex) {
-        String lower = regex.toLowerCase();
-        String REPLACE_ME = lower.replaceAll("[-+.^:,]","");
-        return null;
+        Iterator<String> it = wordList.iterator();
+        Pattern pattern = Pattern.compile(regex);
+        while (it.hasNext()) {
+            String str = it.next();
+            String lower = str.toLowerCase();
+            Matcher matcher = pattern.matcher(lower);
+            if (matcher.matches()) {
+                it.remove();
+            }
+            wordList.add(lower);
+        }
+
+        return wordList;
     }
 
     @Override
     public void printWords(Map<String, ArrayList<String>> words) {
+        for (String key : words.keySet()) {
+            ArrayList<String> list = words.get(key);
+            System.out.print(list.size() + "x " + key + ": [");
+            Iterator<String> it = list.iterator();
+            if (it.hasNext()) {
+                System.out.print(it.next());
+            }
+
+            while (it.hasNext()) {
+                System.out.print(", " + it.next());
+            }
+            System.out.print("]\n");
+
+        }
 
     }
 
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
     }
 }
-
