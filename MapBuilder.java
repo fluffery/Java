ed174b07cb0d3c84d16958c7fce56b12a27fa0cf
fluffery <38757807+fluffery@users.noreply.github.com>
2018-05-05 17:43:28 +0200

    Update MapBuilder.java

--- MapBuilder.java
+++ MapBuilder.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.*;
 
 
 public class MapBuilder {
 
     public static HashMap<String, ArrayList<String>> buildTermMap(ArrayList<String> wordList, String[] keys) {
 
-        HashMap<String, ArrayList<String>> myhash = new HashMap<>();
-        String word = readNext(fileName);
+        HashMap<String, ArrayList<String>>myhash = new HashMap<String,ArrayList<String>>();
         for (String key : keys) {
             myhash.put(key, wordList);
-
+            String word = wordList.iterator().next();
             if (word.contains(key)) {
                 if (!wordList.contains(word)) {
                     wordList.add(word);
@@ -22,11 +20,36 @@ public class MapBuilder {
 
             }
         }
+
         return myhash;
     }
 
     public static HashMap<String, ArrayList<String>> buildAlphaMap(ArrayList<String> wordList){
+        HashMap<String, ArrayList<String>> hash = new HashMap<String,ArrayList<String>>();
+        Iterator<String> it= wordList.iterator();
+        String [] keys= new String[wordList.size()];
+        int a = 0;
+        while(it.hasNext()) {
+            String letter= it.next().trim().substring(0, 1);
+            keys[a]= letter;
+            a++;
+        }
+        Arrays.sort(keys);
+        for(int i=0;i<keys.length;i++) {
+            Iterator<String> it2 = wordList.iterator();
+            ArrayList<String> list= new ArrayList<String>();
+            while(it.hasNext()) {
+                String words= it.next();
+                if(words.contains(keys[i])) {
+                    list.add(words);
+                }
+            }
+            Collections.sort(list);
+            buildTermMap.put(keys[i], list);
+        }
+        return buildTermMap;
 
     }
 
 }
+
